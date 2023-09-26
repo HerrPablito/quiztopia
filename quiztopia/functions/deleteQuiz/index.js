11221f02c0325ae25fad5b0e2a9141bf845a0511
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-09-26 13:47:16 +0200

    save
    save

--- quiztopia/functions/deleteQuiz/index.js
+++ quiztopia/functions/deleteQuiz/index.js
@@ -5,25 +5,48 @@ const jwt = require('jsonwebtoken');
 const { validateToken } = require('../../middleware/auth');
 
 async function deleteQuiz(params, context) {
-  try {    
-    
-    console.log("här: " + context.userName);
+    const { quizId } = params;
     const userName = context.userName;
     
-        params = {
-        TableName: process.env.QUIZ_NAME,
-        Key: {
-            quizId: quizId
-        }
+    
+    //console.log("eller här: " + params);  
+    try {    
+        const { Items } = await db.scan({
+            TableName: "eventsDb",
+            FilterExpression: "attribute_exists(#DYNOBASE_artist)",
+            ExpressionAttributeNames: {
+                "#DYNOBASE_artist": "artist"
+            }
+            
+        }).promise();
+        myQuiz = JSON.stringify(Items);
+        console.log("Items: " + myQuiz);         
+    //     params = {
+    //     TableName: process.env.QUIZ_TABLE,
+    //     Key: {
+    //         quizId: quizId
+    //     }
+    // }
+    // console.log("nr1: " + params.createdBy);
+    // console.log("nr2: " + quiz.Item.createdBy);
+    // console.log("nr3: " + Item.createdBy);
+    //console.log("nr4: " + params);
+    if (userName !== quiz.Item.createdBy) {
+        return sendError(403, { success: false, message: "You are not authorized to delete this quiz" });
     }
-
-
-    const quiz = await db.get(params).promise();
+            await db.delete({
+            TableName: "quizDb",
+            Key: {
+                quizId: quizId
+            }
+        }).promise();
+    return sendResponse(200, { success: true, message: "Quiz deleted" });
+    //const quiz = await db.get(params).promise();
 
     if (!quiz.Item) {
       return sendError(404, { success: false, message: "Quiz not found" });
     }
-    console.log(quiz.Item.createdBy);
+    //console.log(quiz.Item.createdBy);
   }
  catch (error) {
     console.log(error);
@@ -38,7 +61,10 @@ module.exports.handler = middy()
     .use(validateToken)
     .handler(async (event, context) => {
     try {
-        return await deleteQuiz(JSON.parse(event.pathParameters), context);
+       // console.log("typ" + typeof event.pathParameters);
+        //console.log(event.pathParameters);
+        return await deleteQuiz(event.pathParameters.quizId, context);
+        
     } catch (error) {
         console.log(error);
         return sendError(500, { success: false, message: "could not delete quiz" });
